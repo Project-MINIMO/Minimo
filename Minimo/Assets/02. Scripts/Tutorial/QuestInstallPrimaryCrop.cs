353934be0a3e911a9fbc55aa0f1a55ba1842ecb8
dah <daun792@gmail.com>
2025-02-17 22:42:29 +0900

    [Fix] 건물 설치하지도 않았는데 퀘스트 완료되던 문제 수정

--- Minimo/Assets/02. Scripts/Tutorial/QuestInstallPrimaryCrop.cs
+++ Minimo/Assets/02. Scripts/Tutorial/QuestInstallPrimaryCrop.cs
@@ -9,6 +9,8 @@ public class QuestInstallPrimaryCrop : QuestBase
     [SerializeField] private Transform _builidngParent;
     [SerializeField] private TextMeshProUGUI _detailText;
     
+    private BuildingObject _buildingObject;
+    
     protected override void ClearQuest()
     {
         base.ClearQuest();
@@ -18,17 +20,29 @@ public class QuestInstallPrimaryCrop : QuestBase
     
     private bool CheckClear()
     {
-        if (_builidngParent.childCount > 0)
+        if (_buildingObject == null) 
         {
-            for (var i = 0; i < _builidngParent.childCount; i++)
+            if (_builidngParent.childCount > 0)
             {
-                if (string.Equals(_builidngParent.GetChild(i).gameObject.name, "Building_Farm(Clone)"))
+                for (var i = 0; i < _builidngParent.childCount; i++)
                 {
-                    return true;
+                    if (string.Equals(_builidngParent.GetChild(i).gameObject.name, "Building_Farm(Clone)"))
+                    {
+                        _buildingObject = _builidngParent.GetChild(i).gameObject.GetComponent<BuildingObject>();
+                    }
                 }
             }
+            
+            return false;
         }
-
+        else
+        {
+            if (_buildingObject.IsPlaced) 
+            {
+                return true;
+            }
+        }
+        
         return false;
     }
 }
