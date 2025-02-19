414b933cc37362a58a2bbf47fded6c0fe1b950bd
HaramK <37320983+HaramK@users.noreply.github.com>
2025-02-19 23:08:36 +0900

    [수정] 정말 멍청하다..

--- Minimo_Server/Minimo_Server/Program.cs
+++ Minimo_Server/Minimo_Server/Program.cs
@@ -189,7 +189,7 @@ namespace MinimoServer
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:8080");
+                    webBuilder.UseUrls("http://0.0.0.0:8080");
                 });
     }
 }
