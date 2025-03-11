6fddc639d113efd8477691c0605332b8843e2c1a
dah <daun792@gmail.com>
2025-03-12 04:39:30 +0900

    [Add] 빌딩 리소스 제작 툴 추가

---
+++ Minimo/Assets/02. Scripts/Enum/BuildingPositionData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "NewBuildingData", menuName = "Building Position Data")]
+public class BuildingPositionData : ScriptableObject
+{
+    public string Code;  
+    public List<Vector2Int> GroundTilePositions = new(); 
+    public List<Vector2Int> WaterTilePositions = new();  
+    public Vector2 Offset;
+    public Sprite Sprite;
+}
