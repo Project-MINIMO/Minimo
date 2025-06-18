e07040f23aa0a5b47f3f3c9bb0171d68aa859382
dah <daun792@gmail.com>
2025-06-18 22:20:36 +0900

    [Mod] 오류 로그 수정

--- Minimo/Assets/02. Scripts/UI/Produce/ItemInfoUpdater.cs
+++ Minimo/Assets/02. Scripts/UI/Produce/ItemInfoUpdater.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class ItemInfoUpdater : MonoBehaviour
 {
@@ -36,13 +37,21 @@ public class ItemInfoUpdater : MonoBehaviour
             }
             
             _infoTMP[i].text = $"X{_currentOption.ResultItems[i].Amount}";
-            _infoImages[i]?.sprite = Resources.Load<Sprite>($"Item/{itemData.Name}");
-            _infoImages[i]?.gameObject.SetActive(true);
+
+            if (_infoImages[i] != null)
+            {
+                _infoImages[i].sprite = Resources.Load<Sprite>($"Item/{itemData.Name}");
+                _infoImages[i].gameObject.SetActive(true);
+            }
         }
 
         for (; i < _infoImages.Length; i++) 
         {
-            _infoImages[i]?.gameObject.SetActive(false);
+            if (_infoImages[i] != null)
+            {
+                _infoImages[i]?.gameObject.SetActive(false);
+            }
+            
         }
     }
 
