6911cecfaaf42d53209faec4a2fa66153e016c5e
dah <daun792@gmail.com>
2025-08-05 19:22:06 +0900

    [Refactor] 타일 UI 리펙토링

--- Minimo/Assets/02. Scripts/UI/Tile/ITileBrush.cs
+++ Minimo/Assets/02. Scripts/UI/Tile/ITileBrush.cs
@@ -1,48 +1,79 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public struct TileBrushResult
+{
+    public bool Success;
+    public TileChange Change; 
+    
+    public static TileBrushResult Failed => new TileBrushResult { Success = false };
+}
+
 public interface ITileBrush
 {
-    bool Apply(Vector3Int cell);
+    public CustomTile SelectedTile { get; }
+    TileBrushResult Apply(Vector3Int cell);
 }
 
 public class PaintBrush : ITileBrush
 {
     private readonly Tilemap _tilemap;
     private readonly Tilemap _glowMap;
-    private CustomTile _selectedTile;
+    private readonly Tilemap _installMap;
+    public CustomTile SelectedTile { get; private set; }
 
-    public PaintBrush(Tilemap tilemap, Tilemap glowMap)
+    public PaintBrush(Tilemap tilemap, Tilemap glowMap, Tilemap installMap)
     {
         _tilemap = tilemap;
         _glowMap = glowMap;
+        _installMap = installMap;
     }
     
     public void SetSelectedTile(CustomTile tile)
     {
-        _selectedTile = tile;
+        SelectedTile = tile;
     }
-
-    public bool Apply(Vector3Int cell)
+    
+    public TileBrushResult Apply(Vector3Int cell)
     {
-        if (_selectedTile == null)
+        if (SelectedTile == null)
         {
             App.Notification(NotifyType.DeselectTile);
-            return false;
+            return TileBrushResult.Failed;
+        }
+
+        if (_tilemap.GetTile(cell) == SelectedTile.Tile)
+        {
+            return TileBrushResult.Failed;
         }
         
-        if (!_selectedTile.CanInstall())
+        if (_installMap.GetTile(cell) != null && SelectedTile.Type == TileType.Water)
+        {
+            App.Notification(NotifyType.CannotInstallWaterTile);
+            return TileBrushResult.Failed;
+        }
+        
+        if (!SelectedTile.CanInstall())
         {
             App.Notification(NotifyType.GoldLack);
-            return false;
+            return TileBrushResult.Failed;
         }
         
-        AccountInfo.Instance.Gold.AddCount(-_selectedTile.Cost);
+        AccountInfo.Instance.Gold.AddCount(-SelectedTile.Cost);
 
-        _tilemap.SetTile(cell, _selectedTile.Tile);
-        _glowMap.SetTile(cell, _selectedTile.Tile);
+        _tilemap.SetTile(cell, SelectedTile.Tile);
+        _glowMap.SetTile(cell, SelectedTile.Tile);
 
-        return true;
+        return new TileBrushResult
+        {
+            Success = true,
+            Change = new TileChange
+            {
+                ChangeType = TileChangeType.Install,
+                TileId = SelectedTile.ID,
+                Position = cell
+            }
+        };
     }
 }
 
@@ -51,6 +82,7 @@ public class EraseBrush : ITileBrush
     private readonly Tilemap _tilemap;
     private readonly Tilemap _glowMap;
     private readonly Tilemap _installMap;
+    public CustomTile SelectedTile { get; private set; }
 
     public EraseBrush(Tilemap tilemap, Tilemap glowMap, Tilemap installMap)
     {
@@ -59,17 +91,31 @@ public class EraseBrush : ITileBrush
         _installMap = installMap;
     }
 
-    public bool Apply(Vector3Int cell)
+    public TileBrushResult Apply(Vector3Int cell)
     {
+        if (_tilemap.GetTile(cell) == null)
+        {
+            return TileBrushResult.Failed;
+        }
+        
         if (_installMap.GetTile(cell) != null)
         {
             App.Notification(NotifyType.CannotEraseTile);
-            return false;
+            return TileBrushResult.Failed;
         }
         
         _tilemap.SetTile(cell, null);
         _glowMap.SetTile(cell, null);
 
-        return true;
+        return new TileBrushResult
+        {
+            Success = true,
+            Change = new TileChange
+            {
+                ChangeType = TileChangeType.Remove,
+                TileId = -1,
+                Position = cell
+            }
+        };
     }
 }
--- Minimo/Assets/02. Scripts/UI/Tile/TilePanel.cs
+++ Minimo/Assets/02. Scripts/UI/Tile/TilePanel.cs
@@ -12,7 +12,10 @@ public class TilePanel : UIBase
     [SerializeField] private TextMeshProUGUI _titleTMP;
 
     [SerializeField] private Button _openBtn;
-    [SerializeField] private Button _closeBtn;
+    [SerializeField] private Button _closeBtn; 
+    [SerializeField] private Button _confirmBtn;
+    [SerializeField] private Button _cancelBtn;  
+    
     [SerializeField] private Image _selectedTileImg;
     [SerializeField] private Sprite _eraseSprite;
     [SerializeField] private Toggle _eraseTog;
@@ -26,21 +29,17 @@ public class TilePanel : UIBase
     private Tilemap _tilemap;
     private Tilemap _glowMap;
     private Tilemap _installMap;
-    private CustomTile _selectedTile;
-    private TileBase _tile;
-    
-    private bool _isErase;
-    private bool _isPainting;
-    
-    private Vector3Int _lastPaintedCell = new(int.MinValue, int.MinValue, int.MinValue);
     
     private Dictionary<Vector3Int, (TileBase origTile, TileBase origGlow)> _backupTiles = new();
-
     private List<TileChange> _pendingTileChanges = new();
-
-    [SerializeField] private Button _confirmBtn;
-    [SerializeField] private Button _cancelBtn;
     
+    private Vector3Int _lastPaintedCell = new(int.MinValue, int.MinValue, int.MinValue);
+    private bool _isPainting;
+    
+    private PaintBrush _paintBrush;
+    private EraseBrush _eraseBrush;
+    private ITileBrush _currentBrush;
+
     public override void Initialize(UIManager manager)
     {
         base.Initialize(manager);
@@ -49,6 +48,9 @@ public class TilePanel : UIBase
         _tilemap = GameObject.FindWithTag("VillageTilemap").GetComponent<Tilemap>();
         _glowMap = GameObject.FindWithTag("GlowTilemap").GetComponent<Tilemap>();
         _installMap = GameObject.FindWithTag("InstallTilemap").GetComponent<Tilemap>();
+        
+        _paintBrush = new PaintBrush(_tilemap, _glowMap, _installMap);
+        _eraseBrush = new EraseBrush(_tilemap, _glowMap, _installMap);
         
         _openBtn.onClick.AddListener(OpenPanel);
         _closeBtn.onClick.AddListener(ClosePanel);
@@ -61,24 +63,20 @@ public class TilePanel : UIBase
             slot.OnItemSelected += OnItemSelected;
         }
         
+        var toggles = GetComponentsInChildren<Toggle>(true);
+        foreach (var toggle in toggles)
+        {
+            toggle.onValueChanged.AddListener((isOn) =>
+            {
+                if (!isOn) _currentBrush = null;
+            });
+        }
+        
         _eraseTog.onValueChanged.AddListener((isOn) =>
         {
-            if (isOn)
-            {
-                _tile = null;
-                _selectedTile = null;
-                _isErase = true;
-                _selectedTileImg.sprite = _eraseSprite;
-                _selectedTileImg.gameObject.SetActive(true);
-                _slideHandler.Close();
-                _selectedObj.SetActive(true);
-            }
-            else
-            {
-                _isErase = false;
-                _selectedTileImg.sprite = null;
-                _selectedTileImg.gameObject.SetActive(false);
-            }
+            if (!isOn) return;
+            _selectedTileImg.sprite = _eraseSprite;
+            SetBrush(_eraseBrush);
         });
 
         _titleTMP.text = App.GetData<TitleData>().GetString("STR_TILEEDIT_NAME");
@@ -90,11 +88,6 @@ public class TilePanel : UIBase
         
         _backupTiles.Clear();
         
-        _isErase = false;
-        _tile = null;
-        _selectedTile = null;
-        _selectedTileImg.sprite = null;
-        _selectedTileImg.gameObject.SetActive(false);
         _toggleGroup.Show(true);
         _selectedObj.SetActive(false);
         
@@ -110,7 +103,7 @@ public class TilePanel : UIBase
         
         _editManager.SetTileEditing(false);
     }
-
+    
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
@@ -118,12 +111,12 @@ public class TilePanel : UIBase
             _isPainting = true;
             TryPaint();
         }
-        else if (Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonUp(0) && _isPainting)
         {
             _isPainting = false;
             _lastPaintedCell = new Vector3Int(int.MinValue, int.MinValue, int.MinValue);
         }
-        else if (_isPainting && Input.GetMouseButton(0))
+        else if (Input.GetMouseButton(0) && _isPainting)
         {
             TryPaint();
         }
@@ -143,50 +136,8 @@ public class TilePanel : UIBase
             App.Notification(NotifyType.CannotReplaceTile);
             return;
         }
-        if (_isErase)
-        {
-            if (_installMap.GetTile(cellPos) != null)
-            {
-                App.Notification(NotifyType.CannotEraseTile);
-                return;
-            }
-            
-            if (!_backupTiles.ContainsKey(cellPos))
-            {
-                var origTile = _tilemap.GetTile(cellPos);
-                var origGlow = _glowMap.GetTile(cellPos);
-                _backupTiles[cellPos] = (origTile, origGlow);
-            }
-            
-            if (!_backupTiles.ContainsKey(cellPos))
-            {
-                var origTile = _tilemap.GetTile(cellPos);
-                var origGlow = _glowMap.GetTile(cellPos);
-                _backupTiles[cellPos] = (origTile, origGlow);
-            }
-            
-            // 변경사항 기록 (삭제)
-            int prevTileId = _selectedTile != null ? _selectedTile.ID : -1;
-            _pendingTileChanges.Add(new TileChange
-            {
-                ChangeType = TileChangeType.Remove,
-                TileId = prevTileId,
-                Position = cellPos
-            });
-
-            _tilemap.SetTile(cellPos, null);
-            _glowMap.SetTile(cellPos, null);
-            return;
-        }
         
-        if (_installMap.GetTile(cellPos) != null && _selectedTile.Type == TileType.Water)
-        {
-            App.Notification(NotifyType.CannotInstallWaterTile);
-            return;
-        }
-        
-        if (_tilemap.GetTile(cellPos) == _tile) return;
-        if (!UseGold()) return;
+        if (_currentBrush == null) return;
         
         if (!_backupTiles.ContainsKey(cellPos))
         {
@@ -195,40 +146,23 @@ public class TilePanel : UIBase
             _backupTiles[cellPos] = (origTile, origGlow);
         }
         
-        // 변경사항 기록 (설치)
-        _pendingTileChanges.Add(new TileChange
+        var result = _currentBrush.Apply(cellPos);
+        if (result.Success)
         {
-            ChangeType = TileChangeType.Install,
-            TileId = _selectedTile != null ? _selectedTile.ID : -1,
-            Position = cellPos
-        });
+            _pendingTileChanges.Add(result.Change);
+        }
+    }
 
-        _tilemap.SetTile(cellPos, _tile);
-        _glowMap.SetTile(cellPos, _tile);
-    }
-    
-    private bool UseGold()
-    {
-        if (_selectedTile == null) return true;
-        
-        if (_selectedTile.CanInstall())
-        {
-            AccountInfo.Instance.Gold.AddCount(-_selectedTile.Cost);
-            return true;
-        }
-        else
-        {
-            App.Notification(NotifyType.GoldLack);
-            return false;
-        }
-    }
-    
     private void OnItemSelected(InventorySlot<CustomTile> slot)
     {
-        _tile = slot.Item.Tile;
-        _selectedTile = slot.Item;
+        _paintBrush.SetSelectedTile(slot.Item);
         _selectedTileImg.sprite = slot.Item.Icon;
-        _selectedTileImg.gameObject.SetActive(true);
+        SetBrush(_paintBrush);
+    }
+
+    private void SetBrush(ITileBrush brush)
+    {
+        _currentBrush = brush;
         _slideHandler.Close();
         _selectedObj.SetActive(true);
     }
@@ -236,31 +170,35 @@ public class TilePanel : UIBase
     private async void Confirm()
     {
         _backupTiles.Clear();
-        // 일괄 저장
+
         if (_pendingTileChanges.Count > 0)
         {
             var firebaseManager = App.GetManager<FirebaseManager>();
             await firebaseManager.BatchUpdateTiles(_pendingTileChanges);
             _pendingTileChanges.Clear();
         }
+        
+        _currentBrush = null;
         _slideHandler.Open();
         _selectedObj.SetActive(false);
     }
 
     private void Cancel()
     {
-        foreach (var kv in _backupTiles)
+        foreach (var tile in _backupTiles)
         {
-            var cell = kv.Key;
-            var (origTile, origGlow) = kv.Value;
+            var cell = tile.Key;
+            var (origTile, origGlow) = tile.Value;
             _tilemap.SetTile(cell, origTile);
             _glowMap.SetTile(cell, origGlow);
 
-            if (!_isErase)
+            if (_currentBrush is PaintBrush)
             {
-                AccountInfo.Instance.Gold.AddCount(_selectedTile.Cost);
+                AccountInfo.Instance.Gold.AddCount(_currentBrush.SelectedTile.Cost);
             }
         }
+
+        _currentBrush = null;
         _backupTiles.Clear();
         _slideHandler.Open();
         _selectedObj.SetActive(false);
