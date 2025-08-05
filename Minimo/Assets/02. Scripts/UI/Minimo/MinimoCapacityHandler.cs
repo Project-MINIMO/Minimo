e383b1898c47d5b68e50ec5f79887434917625a8
dah <daun792@gmail.com>
2025-08-06 03:54:00 +0900

    [Fix] 미니모 거주 공간 확장 팝업 버그 수정

--- Minimo/Assets/02. Scripts/UI/Minimo/MinimoCapacityHandler.cs
+++ Minimo/Assets/02. Scripts/UI/Minimo/MinimoCapacityHandler.cs
@@ -1,12 +1,18 @@
-using System;
-using UnityEngine;
-
 public class MinimoCapacityHandler : CapacityHandler
 {
     protected override int GetExpandCost(TitleData title) => title.Common["ResidenceExpandCost"];
     protected override int GetCurrentCapacity() => AccountInfo.Instance.MinimoCapacity;
-    protected override int GetBaseCapacity() => 20;
+    protected override int GetBaseCapacity() => _minimoManager.ActiveMinimos.Count;
 
+    private MinimoManager _minimoManager;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _minimoManager = App.GetManager<MinimoManager>();
+    }
+    
     protected override void SuccessTransaction()
     {
         AccountInfo.Instance.AddMinimoCapacity(Quantity - CurrentCapacity);
