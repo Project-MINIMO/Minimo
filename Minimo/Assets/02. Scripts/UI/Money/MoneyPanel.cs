a4e40e84adede835635dd1cd6eac5df16c8e258e
dah <daun792@gmail.com>
2025-08-07 05:21:35 +0900

    [Mod] 돈 표기 수정

--- Minimo/Assets/02. Scripts/UI/Money/MoneyPanel.cs
+++ Minimo/Assets/02. Scripts/UI/Money/MoneyPanel.cs
@@ -5,22 +5,39 @@ public class MoneyPanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _goldTMP;
     [SerializeField] private TextMeshProUGUI _cashTMP;
-
+    
+    private const string ThousandSuffix = "K";
+    private const string MillionSuffix  = "M";
+    private const string BillionSuffix  = "B";
+    private const string TrillionSuffix = "T";
+    
     private void Awake()
     {
         AccountInfo.Instance.Gold.OnGoldChanged += OnGoldChanged;
         AccountInfo.Instance.OnCashChanged += OnCashChanged;
         OnGoldChanged(AccountInfo.Instance.Gold.Count);
-        OnGoldChanged(AccountInfo.Instance.Cash);
+        OnCashChanged(AccountInfo.Instance.Cash);
     }
     
     private void OnGoldChanged(long value)
     {
-        _goldTMP.SetText(value.ToString());
+        _goldTMP.SetText(FormatNumber(value));
     }
     
     private void OnCashChanged(long value)
     {
-        _cashTMP.SetText(value.ToString());
+        _cashTMP.SetText(FormatNumber(value));
+    }
+    
+    private string FormatNumber(long value)
+    {
+        return value switch
+        {
+            >= 1_000_000_000_000 => (value / 1_000_000_000_000f).ToString("0.#") + TrillionSuffix,
+            >= 1_000_000_000 => (value / 1_000_000_000f).ToString("0.#") + BillionSuffix,
+            >= 1_000_000 => (value / 1_000_000f).ToString("0.#") + MillionSuffix,
+            >= 1_000 => (value / 1_000f).ToString("0.#") + ThousandSuffix,
+            _ => value.ToString()
+        };
     }
 }
