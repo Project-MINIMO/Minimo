d6b9d39ae8fafa0485b8f34fd60f6c03dd17ed30
dah <daun792@gmail.com>
2025-09-19 22:08:19 +0900

    버그수정

--- Minimo/Assets/02. Scripts/Data/Item.cs
+++ Minimo/Assets/02. Scripts/Data/Item.cs
@@ -45,5 +45,13 @@ public class Item : IQuestClearTarget, IQuestRewardTarget
     }
     
     public void SetBuildingID(int buildingID) => BuildingCode = buildingID;
-    public void SetMaterialCodes(int[] materialCodes) => MaterialCodes = materialCodes;
+    public void SetMaterialCodes(int[] materialCodes)
+    {
+        if (Level == 1)
+        {
+            materialCodes = Array.Empty<int>();
+        }
+        
+        MaterialCodes = materialCodes;
+    }
 }
